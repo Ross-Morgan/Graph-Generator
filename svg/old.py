276e501a609be2996920108a4a7e8cf3409183b6
Ross Morgan <morgan.ross.512@gmail.com>
2021-12-09 01:06:09 +0000

    type hints [PEP 484]

--- svg/old.py
+++ svg/old.py
@@ -140,7 +140,7 @@ class SVGWriter:
         # Close svg tag
         self.write_line(self.tag("svg", closing=True))
 
-    def init_svg(self, height, width):
+    def init_svg(self, height: int, width: int):
         self.add_element("svg", {
             "xmlns": "http://www.w3.org/2000/svg",
             "xmlns:ev": "http://www.w3.org/2001/xml-events",
